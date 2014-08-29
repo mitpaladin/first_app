034d88807f3984276ea89eeff9bd598230eb08c1
Charles Du <charles.q.du@theprolog.com>
2014-08-29 16:36:59 +0800

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '1add25aba791ba797eb4e336e34181564764ff1e4a74aaf3ef3a87dedb0f7181bdc8262913462aeffb094e325c7d518ba2c2675aee402b772df726530acb5d37'
